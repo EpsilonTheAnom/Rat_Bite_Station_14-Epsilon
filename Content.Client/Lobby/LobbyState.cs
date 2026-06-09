2869bbbd1eacb7f18b5bf502adf9770783f1eaba
my33222233 <main@my33222233.mozmail.com>
2026-06-09 17:25:39 +0200

    readd guilt tripping
    You VILL feel bad und it VILL get worse!

--- Content.Client/Lobby/LobbyState.cs
+++ Content.Client/Lobby/LobbyState.cs
@@ -305,7 +305,7 @@ namespace Content.Client.Lobby
             UpdatePlayerBalance(); // Goobstation - Goob Coin
 
             var minutesToday = _playtimeTracking.PlaytimeMinutesToday;
-            if (minutesToday > 99999) // trauma, remove guilt tripping
+            if (minutesToday > 60)
             {
                 Lobby!.PlaytimeComment.Visible = true;
 
