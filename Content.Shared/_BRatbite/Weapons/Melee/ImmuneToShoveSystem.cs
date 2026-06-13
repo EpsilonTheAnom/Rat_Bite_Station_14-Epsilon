dafa01675e08d7963bc936200a43e15b052cf4da
Perstronzio Desantis <44839463+PropenzioLavandino@users.noreply.github.com>
2026-06-13 12:07:58 +0200

    Change immune to shove to only disable physics

--- Content.Shared/_BRatbite/Weapons/Melee/ImmuneToShoveSystem.cs
+++ Content.Shared/_BRatbite/Weapons/Melee/ImmuneToShoveSystem.cs
@@ -1,4 +1,5 @@
 using Content.Shared.Weapons.Melee.Events;
+using Content.Shared.Mobs;
 
 namespace Content.Shared._BRatbite.Weapons.Melee;
 
@@ -7,14 +8,27 @@ public sealed class ImmuneToShoveSystem : EntitySystem
     public override void Initialize()
     {
         base.Initialize();
-        SubscribeLocalEvent<ImmuneToShoveComponent, BeforeHarmfulActionEvent>(OnDisarmAttempt);
+        SubscribeLocalEvent<ImmuneToShoveOnDeathComponent, MobStateChangedEvent>(OnMobStateChange);
+        SubscribeLocalEvent<ImmuneToShoveOnDeathComponent, ComponentShutdown>(OnShutdown);
     }
 
-    public void OnDisarmAttempt(Entity<ImmuneToShoveComponent> ent, ref BeforeHarmfulActionEvent args)
+    private void OnMobStateChange(Entity<ImmuneToShoveOnDeathComponent> ent, ref MobStateChangedEvent args)
     {
-        if (args.Type == HarmfulActionType.Disarm)
+        if (args.NewMobState == MobState.Dead)
         {
-            args.Cancel();
+            EnsureComp<ImmuneToShoveComponent>(ent.Owner);
+
+        }
+        else
+        {
+            RemComp<ImmuneToShoveComponent>(ent.Owner);
         }
     }
+
+    private void OnShutdown(Entity<ImmuneToShoveOnDeathComponent> ent, ref ComponentShutdown args)
+    {
+        RemComp<ImmuneToShoveComponent>(ent.Owner);
+    }
+
+
 }
